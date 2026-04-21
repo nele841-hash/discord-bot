0f23bc333395be1f2d875578bbf7c40d58abfe92
nele841-hash <avdija841@gmail.com>
2026-04-21 10:05:45 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,11 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
 from pymongo import MongoClient
 import os
 
